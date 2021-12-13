46c4eee86922c6df7883db0b291ebda112da0e5b
Michael Rivnak <rivnakm1@gmail.com>
2021-12-13 15:46:27 -0500

    added more containers

--- configuration.nix
+++ configuration.nix
@@ -5,107 +5,112 @@
 { config, pkgs, ... }:
 
 {
-  imports = [
-      ./hardware-configuration.nix
-      ./docker-configuration.nix
-      ./snapper-configuration.nix
-      <home-manager/nixos>
-    ];
+	imports = [
+		./hardware-configuration.nix
+		./docker-configuration.nix
+		./snapper-configuration.nix
+		<home-manager/nixos>
+	];
 
-  system.autoUpgrade.enable = false;
-  system.autoUpgrade.allowReboot = false;
+	system.autoUpgrade.enable = false;
+	system.autoUpgrade.allowReboot = false;
 
-  nix = {
-    package = pkgs.nixFlakes;
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
-  };
+	nix = {
+		package = pkgs.nixFlakes;
+		extraOptions = ''
+			experimental-features = nix-command flakes
+		'';
+	};
 
-  boot.supportedFilesystems = [ "btrfs" ];
+	boot.supportedFilesystems = [ "btrfs" ];
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+	# Use the systemd-boot EFI boot loader.
+	boot.loader.systemd-boot.enable = true;
+	boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos-server"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+	networking.hostName = "nixos-server"; # Define your hostname.
+	# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Set your time zone.
-  time.timeZone = "America/New_York";
+	# Set your time zone.
+	time.timeZone = "America/New_York";
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.enp2s0.useDHCP = true;
+	# The global useDHCP flag is deprecated, therefore explicitly set to false here.
+	# Per-interface useDHCP will be mandatory in the future, so this generated config
+	# replicates the default behaviour.
+	networking.useDHCP = false;
+	networking.interfaces.enp2s0.useDHCP = true;
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+	# Select internationalisation properties.
+	i18n.defaultLocale = "en_US.UTF-8";
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.michael = {
-    isNormalUser = true;
-    description = "Michael";
-    shell = pkgs.zsh;
-    extraGroups = [
-      "docker"
-      "wheel"
-    ];
-  };
-  users.users.mig = {
-    isNormalUser = true;
-    description = "Mig";
-    shell = pkgs.zsh;
-    extraGroups = [
-      "docker"
-      "wheel"
-    ];
-  };
+	# Define a user account. Don't forget to set a password with ‘passwd’.
+	users.users.michael = {
+		isNormalUser = true;
+		description = "Michael";
+		shell = pkgs.zsh;
+		extraGroups = [
+			"docker"
+			"wheel"
+		];
+	};
+	users.users.mig = {
+		isNormalUser = true;
+		description = "Mig";
+		shell = pkgs.zsh;
+		extraGroups = [
+			"docker"
+			"wheel"
+		];
+	};
 
-  # List packages installed in system profile. To search, run:
-  environment.systemPackages = with pkgs; [
-    curl
-    git
-    home-manager
-    neovim
-    wget
-  ];
-  nixpkgs.config.allowUnfree = true;
+	# List packages installed in system profile. To search, run:
+	environment.systemPackages = with pkgs; [
+		curl
+		git
+		home-manager
+		neovim
+	openssl
+		wget
+	];
+	nixpkgs.config.allowUnfree = true;
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+	# Some programs need SUID wrappers, can be configured further or are
+	# started in user sessions.
+	# programs.mtr.enable = true;
+	# programs.gnupg.agent = {
+	#   enable = true;
+	#   enableSSHSupport = true;
+	# };
 
-  # List services that you want to enable:
+	# List services that you want to enable:
 
-  # Enable the OpenSSH daemon.
-  services.openssh = {
-    enable = true;
-    # passwordAuthentication = false;
-    extraConfig = ''
-      PrintLastLog no
-    '';
-  };
+	# Enable the OpenSSH daemon.
+	services.openssh = {
+		enable = true;
+		# passwordAuthentication = false;
+		extraConfig = ''
+			PrintLastLog no
+		'';
+	};
 
-  # Open ports in the firewall.
-  networking.firewall.enable = true;
-  networking.firewall.package = pkgs.nftables;
-  networking.firewall.allowedTCPPorts = [
-    25565 # mc-vanilla
-  ];
-  services.fail2ban.enable = true;
+	# Open ports in the firewall.
+	networking.firewall.enable = true;
+	networking.firewall.allowedTCPPorts = [
+		8123  # home assistant
+		21064 # home assistant HomeKit plugin
+		25565 # mc-vanilla
+	];
+	networking.firewall.allowedUDPPorts = [
+		5353  # mDNS
+	];
+	services.fail2ban.enable = true;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "21.11"; # Did you read the comment?
+	# This value determines the NixOS release from which the default
+	# settings for stateful data, like file locations and database versions
+	# on your system were taken. It‘s perfectly fine and recommended to leave
+	# this value at the release version of the first install of this system.
+	# Before changing this value read the documentation for this option
+	# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+	system.stateVersion = "21.11"; # Did you read the comment?
 
 }
